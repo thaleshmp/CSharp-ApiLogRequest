ccefa3dc2aa8c1f460422aa5bcbea323eb92f94e
Thales Henrique Miyashiro Pilao <thales.pilao@fcamara.com.br>
2017-04-20 14:41:32 -0300

    Adding POST request

--- LogRequestApi/Controllers/ValuesController.cs
+++ LogRequestApi/Controllers/ValuesController.cs
@@ -1,39 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 
 namespace LogRequestApi.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
+        [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        public string Get(int id)
+        [HttpPost]
+        public string Post(SimpleModel model)
         {
-            return "value";
+            return "Success";
         }
+    }
 
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
+    public class SimpleModel
+    {
+        public string key { get; set; }
+        public string value { get; set; }
     }
 }
